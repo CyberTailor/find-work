4d862639f6a8e6ea1e997f415a53ce466698eff5
Anna “CyberTailor” <cyber@sysrq.in>
2024-04-28 17:46:15 +0500

    Bump version: 0.6.1 → 0.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 project = 'find-work'
 author = 'Anna (cybertailor) Vyalkova'
 copyright = f'2024, {author}'
-release = '0.6.1'
+release = '0.7.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- find_work/constants.py
+++ find_work/constants.py
@@ -8,7 +8,7 @@
 PACKAGE = "find-work"
 
 # Application version.
-VERSION = "0.6.1"
+VERSION = "0.7.0"
 
 # Application homepage.
 HOMEPAGE = "https://find-work.sysrq.in"
