a4c2e7a0cce3ea8945b48d842289ac7ef2c56677
Anna “CyberTailor” <cyber@sysrq.in>
2024-08-20 22:49:24 +0500

    Bump version: 0.91.0 → 0.91.1

--- find_work/constants.py
+++ find_work/constants.py
@@ -10,7 +10,7 @@ All important constants in one place.
 PACKAGE = "find-work"
 
 #: Application version.
-VERSION = "0.91.0"
+VERSION = "0.91.1"
 
 #: Application homepage.
 HOMEPAGE = "https://find-work.sysrq.in"
